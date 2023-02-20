33b5ab835031d093d7370c0002f66ef3ae3a9905
Leonid Zelov <112327440+LeonidZelov@users.noreply.github.com>
2023-02-20 20:34:40 +0200

    task 1 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const imagesListTemplate = ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image"
+    src="${preview}" 
+    title="${description}" 
+    alt="${description}" />
+    </a>`;
+};
+
+const addImg = galleryItems.map(imagesListTemplate).join('');
+galleryEl.insertAdjacentHTML("afterbegin", addImg);
+
+
+
+galleryEl.addEventListener('click', galleryImgClick );
+
+function galleryImgClick (evt){
+    const imageSelected = evt.target.getAttribute("data-source");
+
+    evt.preventDefault();
+
+    if (!imageSelected){return;}
+
+};
+
+new SimpleLightbox(".gallery a", {captionDelay: 250, showCounter:false});
